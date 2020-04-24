2f7a9b9c032c244cca00e7b188134555bc9ad7c5
Enzuo <potc.zone+git@gmail.com>
2020-04-24 23:08:22 +0200

    Add static middleware

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
