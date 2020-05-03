ece4692b21338d2ee8676f0d2a7d95a94aee9dcb
Enzuo <potc.zone+git@gmail.com>
2020-05-03 20:01:02 +0200

    Calculate hero score API side

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressip = require('express-ip');
 
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
+app.use(expressip().getIpInfoMiddleware)
 
 app.post('/', function (req, res) {
-  console.log(req.body)
+  var heroesRound = req.body
+  var heroes = heroesRound.h
+  var roundTime = heroesRound.rT
+  var idUser = heroesRound.u
+  var ipUser = req.ipInfo
+  console.log(heroes, roundTime, idUser, ipUser)
+
+  // prevent combo/system abuse
+
+  // save log
+
+  // calculate added score
+  var heroesArr = []
+  for (let [key, value] of Object.entries(heroes)) {
+    var idHero = key
+    if(value.s){
+      score = 3
+      if(roundTime < 3000 ){
+        score += 1
+      }
+    }
+    else {
+      score = -2
+      if(value.hT > 2000 ){
+        score += 1
+      }
+    }
+    heroesArr.push(idHero)
+    heroesArr.push(score)
+  }
+  console.log(heroesArr)
+
+  // save total
+
   res.status(200).send('ok')
 })
 
